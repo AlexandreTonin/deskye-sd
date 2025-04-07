8c37e66e4543d3837f5795eb9e4cdde566896bdc
Alexandre Tonin <xandetonin@gmail.com>
2025-04-06 22:59:05 -0300

    feat: add user routes and configure express middleware in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import pinoHttp from 'pino-http';
 import { logger } from './config/logger/logger.js';
+import { userRoutes } from './routes/userRoutes.js';
 
 const app = express();
 
+// express configs
 app.set('trust proxy', true);
 
+// middlewares
+app.use(express.json());
 app.use(pinoHttp({ logger }));
+
+// routes
+app.use('/users', userRoutes);
 
 export { app };
