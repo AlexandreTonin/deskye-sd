14f488b029eb549d49d4311aaf0da2559423c0e1
Alexandre Tonin <xandetonin@gmail.com>
2025-04-06 23:43:24 -0300

    feat: enhance User model constructor to include additional properties for better user management

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,5 +1,35 @@
 class User {
-  constructor({}) {}
+  constructor({
+    id,
+    name,
+    email,
+    hashedPassword,
+    role,
+    teamId,
+    avatarUrl,
+    lastLoginAt,
+    emailVerifiedAt,
+    createdAt,
+    createdBy,
+    updatedAt,
+    updatedBy,
+    deactivatedAt,
+  }) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.hashedPassword = hashedPassword;
+    this.role = role;
+    this.teamId = teamId;
+    this.avatarUrl = avatarUrl;
+    this.lastLoginAt = lastLoginAt;
+    this.emailVerifiedAt = emailVerifiedAt;
+    this.createdAt = createdAt;
+    this.createdBy = createdBy;
+    this.updatedAt = updatedAt;
+    this.updatedBy = updatedBy;
+    this.deactivatedAt = deactivatedAt;
+  }
 }
 
 export { User };
