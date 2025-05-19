cad6abbd31c6b15356040daecc19cc037079849f
alexandre tonin <xandetonin@gmail.com>
2025-05-19 10:51:10 -0300

    feat: add user validation schema with zod for creating new users

---
+++ backend/src/http/validators/userValidator.js
@@ -0,0 +1,23 @@
+import { z } from 'zod';
+
+const createUserSchema = z.object({
+  name: z.string().min(1, 'Name is required'),
+  email: z.string().email('Invalid Email Address'),
+  password: z
+    .string()
+    .min(8, 'Password must contain at least 8 characters')
+    .max(64, 'Password must have a maximum of 64 characters')
+    .refine((val) => /[A-Z]/.test(val), {
+      message: 'Password must contain at least one uppercase letter.',
+    })
+    .refine((val) => /[0-9]/.test(val), {
+      message: 'Password must contain at least one number.',
+    })
+    .refine((val) => /[!@#$%^&*(),.?":{}|<>]/.test(val), {
+      message: 'Password must contain at least one special character.',
+    }),
+  role: z.enum(['admin', 'agent', 'customer']),
+  teamId: z.number().int(),
+});
+
+export { createUserSchema };
